b033e1054d43926c48bc07681c6d7c886ee177cd
Justin Brower <jbrower95@gmail.com>
2024-10-02 11:09:15 -0400

    todos for forge

--- src/commands/deploy/script/forgeBase.ts
+++ src/commands/deploy/script/forgeBase.ts
@@ -16,7 +16,7 @@ export class ForgeScriptUpgrade<
      * You may use `walletClient` freely -- any transactions sent will be queued for later and signed with the associated signing mechanism.
      */
     async create(_walletClient: WalletClient<TTransport, TNetwork, TAccount, TRPCSchema>, _context: TUpgradeContext<TUCContext, TUCMeta>): Promise<void> {
-        
+        // TODO: callout via forge cli.
     }
 
     /**
@@ -24,7 +24,7 @@ export class ForgeScriptUpgrade<
      * Purpose: queue any multisig changes which involve modifying existing contracts.
      */
     async queue(_walletClient: WalletClient<TTransport, TNetwork, TAccount, TRPCSchema>, _context: TUpgradeContext<TUCContext, TUCMeta>): Promise<void> {
-
+        // TODO: callout via forge cli.
     }
 
     /**
@@ -34,6 +34,6 @@ export class ForgeScriptUpgrade<
      *  - If you had miscellaneous changes to make that didn't require a separate creation + timelock, you can do that here.
      */
     async execute(walletClient: WalletClient<TTransport, TNetwork, TAccount, TRPCSchema>, context: TUpgradeContext<TUCContext, TUCMeta>): Promise<void> {
-
+        // TODO: callout via forge cli.
     }
 }
