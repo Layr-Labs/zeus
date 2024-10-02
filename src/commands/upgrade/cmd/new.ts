6749057185c3ed52559338a73581d07f7762c25b
Justin Brower <jbrower95@gmail.com>
2024-10-02 11:50:31 -0400

    more scaffolding

--- src/commands/upgrade/cmd/new.ts
+++ src/commands/upgrade/cmd/new.ts
@@ -1,9 +1,29 @@
 import {command, positional, string} from 'cmd-ts';
 import {json} from '../../common.js';
 import { requiresLogin, TState } from '../../inject.js';
+import { select } from '@inquirer/prompts';
 
-const handler = async function(user: TState, args: {env: string | undefined, json: boolean}) {
+const handler = async function(user: TState, args: {json: boolean}) {
     // TODO: implement creating a new upgrade.
+
+    const answer = await select({
+        message: 'Select an upgrade strategy',
+        choices: [
+          {
+            name: 'Solidity (Forge Script)',
+            value: 'forge',
+            description: 'write a structured forge script, expressing your upgrade with vm.startBroadcast().',
+          },
+          {
+            name: 'TypeScript (Viem Script)',
+            value: 'viem',
+            description: 'extend a Typescript class, expressing your upgrade with viem contract calls.',
+          },
+        ],
+      });
+    
+      // TODO: implement
+      throw new Error(`${answer} unsupported.`);
 };
 
 const cmd = command({
@@ -11,7 +31,6 @@ const cmd = command({
     description: 'register a new upgrade',
     version: '1.0.0',
     args: {
-        env: positional({ type: string, displayName: 'env' }),
         json,
     },
     handler: requiresLogin(handler),
