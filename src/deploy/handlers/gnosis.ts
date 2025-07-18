0188cedf2345d0d5150265ee05bc4635737e8e6f
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-07-18 12:15:49 -0400

    chore: fix (#52)
    1/n Msig was incorrectly updated to have the wrong order. This fixes the issue

--- src/deploy/handlers/gnosis.ts
+++ src/deploy/handlers/gnosis.ts
@@ -159,12 +159,9 @@ export async function executeMultisigPhase(deploy: SavebleDocument<TDeploy>, met
                     (deploy._.metadata[deploy._.segmentId] as MultisigMetadata).confirmed = true;
                     (deploy._.metadata[deploy._.segmentId] as MultisigMetadata).immediateExecutionHash = sigRequest.immediateExecution.transaction;
                     console.log(`Transaction recorded: ${sigRequest.immediateExecution.transaction}`)
-                    // We need to save and commit before advancing directly to the next segment.
-                    // Otherwise, the metatxn will not be committed
+                    await advanceSegment(deploy);
                     await deploy.save();
                     await metatxn.commit(`[deploy ${deploy._.name}] multisig transaction completed instantly`);
-                    await advanceSegment(deploy);
-                   
                 } else {
                     await advance(deploy);
                     await deploy.save();
